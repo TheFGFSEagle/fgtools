6a9aa63fd53e81a62f3e88560592ce6a7c3ec80b
TheFGFSEagle <thefgfseagle@gmail.com>
2023-11-10 03:45:17 +0100

    Set module version from package version

--- fgtools/utils/constants.py
+++ fgtools/utils/constants.py
@@ -7,6 +7,6 @@ from appdirs import user_cache_dir
 HOME = os.environ.get("HOME", os.path.expanduser("~"))
 CACHEDIR = os.environ.get("FGTOOLS_CACHEDIR", user_cache_dir("fgtools", "TheEagle"))
 os.makedirs(CACHEDIR, exist_ok=True)
-__version__ = (1, 0, 0)
-__versionstr__ = ".".join(map(str, __version__))
+__versionstr__ = version("fgtools")
+__version__ = __versionstr__.split(".")
 
