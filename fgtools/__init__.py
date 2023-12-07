20bc0d0cd4fd74d6b3f9962aa047cf78cd1426ca
TheFGFSEagle <thefgfseagle@gmail.com>
2023-12-07 02:52:48 +0100

    Fix log messages not printing

--- fgtools/__init__.py
+++ fgtools/__init__.py
@@ -3,6 +3,14 @@
 
 import logging
 
+logger = logging.getLogger("fgtools")
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(levelname)s] %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 def get_logger():
 	return logging.getLogger("fgtools")
 
